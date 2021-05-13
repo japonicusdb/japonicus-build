817cd09c7e2b98ecd3972bfe64b787364c4b5d5e
Kim Rutherford <kmr@aska.gen.nz>
2021-05-13 18:36:49 +1200

    Don't use curl to retrieve pombe genes files
    We were getting: curl: (16) Error in the HTTP2 framing layer

--- load-all.sh
+++ load-all.sh
@@ -110,11 +110,10 @@ $JBASE_HOME/pombase-chado/script/pombase-import.pl $LOAD_CONFIG organisms \
 
 echo loading pombe genes
 
-curl https://curation.pombase.org/dumps/latest_build/misc/gene_IDs_names_products.tsv |
 $JBASE_HOME/pombase-chado/script/pombase-import.pl $LOAD_CONFIG features \
     --organism-taxonid=4896 --uniquename-column=1 --name-column=3 --feature-type=gene \
     --product-column=5 --ignore-short-lines \
-    "$HOST" $DB $USER $PASSWORD
+    "$HOST" $DB $USER $PASSWORD < /var/www/pombase/dumps/latest_build/misc/gene_IDs_names_products.tsv
 
 
 
