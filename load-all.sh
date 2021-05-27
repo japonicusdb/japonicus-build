cf94de9aa424da05e0055af3aa56253dd4918904
Kim Rutherford <kmr@aska.gen.nz>
2021-05-27 21:57:13 +1200

    Disable curl http2 to avoid bug
    "curl: (16) Error in the HTTP2 framing layer"
    when connecting to curation.pombase.org

--- load-all.sh
+++ load-all.sh
@@ -215,7 +215,7 @@ $JBASE_HOME/pombase-chado/script/pombase-process.pl \
 
 echo transfer GO annotation from pombe
 
-curl https://curation.pombase.org/dumps/latest_build/pombase-latest.gaf.gz |
+curl --http1.1 https://curation.pombase.org/dumps/latest_build/pombase-latest.gaf.gz |
     gzip -d |
     $POMBASE_CHADO/script/pombase-file-process.pl $LOAD_CONFIG transfer-gaf-annotations \
        --source-organism-taxonid=4896 --dest-organism-taxonid=4897 \
