8dbbd4e40fbce1e5c242b453ffa11d34d3aedb65
Kim Rutherford <kmr@aska.gen.nz>
2021-06-02 13:05:34 +1200

    Load the Rhind orthologs

--- load-all.sh
+++ load-all.sh
@@ -192,11 +192,17 @@ gzip -d < $CURRENT_GOA_GAF | rg '\ttaxon:(4897|402676)\t' |
        --assigned-by-filter=EnsemblFungi,GOC,RNAcentral,InterPro,UniProtKB,UniProt "$HOST" $DB $USER $PASSWORD
 
 
-echo load pombe orthologs
+echo load Compara pombe orthologs
 
 $POMBASE_CHADO/script/pombase-import.pl $LOAD_CONFIG orthologs \
   --publication=PMID:26896847 --organism_1_taxonid=4897 --organism_2_taxonid=4896 \
-  "$HOST" $DB $USER $PASSWORD < $JBASE_HOME/japonicus-curation/pombe_orthologs.tsv 2>&1 | tee $LOG_DIR/$log_file.pombe_orthologs
+  "$HOST" $DB $USER $PASSWORD < $JBASE_HOME/japonicus-curation/compara_pombe_orthologs.tsv 2>&1 | tee $LOG_DIR/$log_file.compara_pombe_orthologs
+
+echo load Rhind pombe orthologs
+
+$POMBASE_CHADO/script/pombase-import.pl $LOAD_CONFIG orthologs \
+  --publication=PMID:21511999 --organism_1_taxonid=4897 --organism_2_taxonid=4896 \
+  "$HOST" $DB $USER $PASSWORD < $JBASE_HOME/japonicus-curation/rhind_pombe_orthologs.tsv 2>&1 | tee $LOG_DIR/$log_file.rhind_pombe_orthologs
 
 
 echo load cerevisiae orthologs
