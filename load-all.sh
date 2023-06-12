e917ba77d31c3df10fedb353390b5bd773dd3f84
Kim Rutherford <kmr@aska.gen.nz>
2023-06-13 00:18:12 +1200

    Rename filtered_GO_IDs file
    Refs japonicusdb/japonicus-curation#54

--- load-all.sh
+++ load-all.sh
@@ -299,7 +299,7 @@ gzip -d < $WWW_DIR/dumps/latest_build/pombase-latest.gaf.gz |
        --evidence-codes-to-ignore=ND --terms-to-ignore="GO:0005515" \
        "$HOST" $DB $USER $PASSWORD |
     $POMBASE_CHADO/script/pombase-import.pl $LOAD_CONFIG gaf \
-       --term-id-filter-filename=$JAPONICUS_CURATION/inferred_from_pombe_filtered_GO_IDs \
+       --term-id-filter-filename=$JAPONICUS_CURATION/japonicusdb_only_filtered_GO_IDs \
        --taxon-filter=4897 "$HOST" $DB $USER $PASSWORD \
        2>&1 | tee $LOG_DIR/$log_file.transfer_pombe_go_annotation
 
